76270cc6ff257632e6264e3c8fef513ad194cea7
Thorsten Siegmund Bartel <thorstensiegmund.bartel@ruhr-uni-bochum.de>
2023-11-27 14:53:53 +0100

    Add option to invert role selection for trigger.

--- lang/en/lifecycletrigger_byrole.php
+++ lang/en/lifecycletrigger_byrole.php
@@ -23,6 +23,8 @@
 $string['pluginname'] = 'Delete courses by roles missing';
 $string['delay'] = 'Days of delay for triggering';
 $string['delay_help'] = 'Days a course has to remain without any responsible person until the course is finally triggered';
+$string['invert'] = 'Invert role selection for triggering.';
+$string['invert_help'] = 'If ticked, any of the selected roles have to be present for a course to be triggered.';
 $string['responsibleroles'] = 'Responsible Roles in courses';
 $string['responsibleroles_help'] = 'Select the roles that have to be presented within a course. If one of the roles is present the course is not triggered.';
 $string['privacy:metadata'] = 'Does not store user specific data';
